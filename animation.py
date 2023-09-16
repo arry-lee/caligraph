f740566336fbc8abab2a4e309229a38d1294453c
arry-lee <arry_lee@qq.com>
2023-09-17 01:15:36 +0800

    分离生成和写入视频

--- animation.py
+++ animation.py
@@ -88,9 +88,9 @@ def find_min_angle_vertices(pts):
         d = (num_vertices) // 2
         # 多个锐角的话，找到最小的和与最小的距离最远的一个
         angles.sort(
-            key=lambda x: (abs(x[1] - si),x[0])  # 坐标到最小点,坐标距离可能是一样的此时用最小的角
+            key=lambda x: (abs(x[1] - si), x[0])  # 坐标到最小点,坐标距离可能是一样的此时用最小的角
             if d >= abs(x[1] - si)
-            else (num_vertices - abs(x[1] - si),x[0]),
+            else (num_vertices - abs(x[1] - si), x[0]),
             reverse=True,
         )
         return [a, angles[0]]
@@ -114,7 +114,21 @@ def find_center_radius(points1, points2):
     # 计算中点
     midpoint = (points1 + closest_points) / 2
 
-    return np.array(midpoint, np.int32), np.array(min_distances / 2, np.int32),closest_points
+    return np.array(midpoint, np.int32), np.array(min_distances / 2, np.int32), closest_points
+
+
+def find_start_end_point(points, stroke_code):
+    """找到points中的起点和终点index"""
+    angles = find_min_angle_vertices(np.array(points, np.int32))
+    s, e = angles[0][1], angles[1][1]
+    # assert points[s] in contours and points[e] in contours
+    if stroke_code == "1":
+        if points[s][0] >= points[e][0]:
+            s, e = e, s
+    else:
+        if points[s][1] >= points[e][1]:  # 除了横以外，一般情况y坐标大的为终点
+            s, e = e, s
+    return s, e
 
 
 def animation(char, fp=None, approx=False):
@@ -197,20 +211,19 @@ def animation(char, fp=None, approx=False):
 
         for i in range(len(curves)):
             # 找到 curves[i] 到 curves2 的最短距离
-            cv2.line(msk, curves[i], closest_points[i], 255, 7)  # todo 半径要随着位置变化
+            # cv2.line(msk, curves[i], closest_points[i], 255, 7)  # todo 半径要随着位置变化
+            # cv2.circle(msk, curves[i], closest_points[i], 255, 7)
             # cv2.line(bg, curves[i], curves[i + 1], (0, 255, 0), 10 + rads[i] * 2)
             # cv2.line(bg, curves[i], curves[i + 1], (255, 0, 0), 2)
             cv2.line(bg, curves[i], closest_points[i], (255, 0, 0), 5)
 
-            # cv2.circle(bg, centers[i], 1 + int(7 * rads[i]), (0, 0, 255), 1)
+            cv2.circle(msk, centers[i], 1 + int(2.2 * rads[i]), 255, -1)
             fr = cv2.bitwise_and(image, msk)
             key_frame = cv2.bitwise_or(key_frame, fr)  # fixme
 
             video_writer.write(cv2.cvtColor(key_frame, cv2.COLOR_GRAY2BGR))
             # 写入gif文件中
             gif_writer.append_data(cv2.cvtColor(key_frame, cv2.COLOR_BGR2RGB))
-            # cv2.imshow("Original Image", bg)
-            # cv2.waitKey(0)
 
         for i in range(len(centers) - 1):
             cv2.line(bg, centers[i], centers[i + 1], (0, 255, 255), 2)
@@ -224,21 +237,88 @@ def animation(char, fp=None, approx=False):
     gif_writer.close()
 
 
-def find_start_end_point(points, stroke_code):
-    """找到points中的起点和终点index"""
-    angles = find_min_angle_vertices(np.array(points, np.int32))
-    s, e = angles[0][1], angles[1][1]
-    # assert points[s] in contours and points[e] in contours
-    if stroke_code == "1":
-        if points[s][0] >= points[e][0]:
-            s, e = e, s
-    else:
-        if points[s][1] >= points[e][1]:  # 除了横以外，一般情况y坐标大的为终点
-            s, e = e, s
-    return s, e
+def generate_frames(char, approx=False):
+    """生成帧"""
+    if len(char) != 1:
+        raise ValueError('only support single character')
+
+    W, H = 150, 150
+    bg = np.zeros((W, H, 3), np.uint8)
+    key_frame = np.zeros((W, H), np.uint8)
+    for stroke_image, stroke_code in zip(char_frames(char, vibe=False)[1:], char_strokes(char)):
+        msk = np.zeros((W, H), np.uint8)
+        mask = stroke_image.point(lambda x: 0 if x > 127 else 255)
+        image = np.asarray(mask, np.uint8)
+
+        path, contours = reconstruct_path(image)
+        points = path[:-1]
+        s, e = find_start_end_point(points, stroke_code)
+        start_point, end_point = points[s], points[e]
+        if approx:
+            if s < e:
+                curves = points[s: e + 1]
+                curves2 = points[e:] + points[: s + 1]
+                curves2.reverse()
+            else:
+                curves = points[s:] + points[: e + 1]
+                curves2 = points[e: s + 1]
+                curves2.reverse()
+        else:
+            s = contours.index(points[s])
+            e = contours.index(points[e])
+            if s < e:
+                curves = contours[s: e + 1]
+                curves2 = contours[e:] + contours[: s + 1]
+                curves2.reverse()
+            else:
+                curves = contours[s:] + contours[: e + 1]
+                curves2 = contours[e: s + 1]
+                curves2.reverse()
+
+        if len(curves) < len(curves2):
+            curves, curves2 = curves2, curves
+
+        centers, rads, closest_points = find_center_radius(np.array(curves), np.array(curves2))
+
+        pts = np.array(path, np.int32)
+        pts = pts.reshape((-1, 1, 2))
+
+        cv2.fillPoly(bg, [pts], (255, 255, 255))
+        cv2.polylines(bg, [pts], True, (255, 0, 0), 1)
+        cv2.circle(bg, start_point, 5, (0, 255, 255))
+        cv2.circle(bg, end_point, 5, (255, 255, 0))
+
+        for i in range(len(curves)):
+            cv2.line(bg, curves[i], closest_points[i], (255, 0, 0), 5)
+            cv2.circle(msk, centers[i], 1 + int(2.2 * rads[i]), 255, -1)
+            fr = cv2.bitwise_and(image, msk)
+            key_frame = cv2.bitwise_or(key_frame, fr)
+            yield key_frame
+
+        for i in range(len(centers) - 1):
+            cv2.line(bg, centers[i], centers[i + 1], (0, 255, 255), 2)
+
+        # yield bg, key_frame
+
+def write_to_file(char, fp=None):
+    """写入文件"""
+    if not fp:
+        fp = f"{char}.mp4"
+
+    W, H = 150, 150
+    fourcc = cv2.VideoWriter_fourcc(*"mp4v")
+    video_writer = cv2.VideoWriter(fp, fourcc, 30.0, (W, H))
+    gif_writer = imageio.get_writer(f"{char}.gif", fps=30)
+
+    for key_frame in generate_frames(char):
+        video_writer.write(cv2.cvtColor(key_frame, cv2.COLOR_GRAY2BGR))
+        gif_writer.append_data(cv2.cvtColor(key_frame, cv2.COLOR_BGR2RGB))
+
+    video_writer.release()
+    gif_writer.close()
 
 
 if __name__ == '__main__':
     import sys
 
-    animation(sys.argv[1])
+    write_to_file(sys.argv[1])

a7dc536833c5c9756f069857970330772f7e0c5d
arry-lee <arry_lee@qq.com>
2023-09-16 22:37:31 +0800

    修复mask不对问题

--- animation.py
+++ animation.py
@@ -8,10 +8,14 @@
 """
 import random
 import cv2
+import imageio
 import numpy as np
 
 from scipy.spatial import distance_matrix
+
+from inscribed_circle import find_inscribed_circle_center
 from strokes import char_frames, char_strokes
+from loguru import logger
 
 
 def reconstruct_path(binary):
@@ -20,7 +24,7 @@ def reconstruct_path(binary):
         binary, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE
     )
 
-    print(len(contours))
+    logger.debug(len(contours))
     # 对轮廓进行简化或拟合，获得路径点列表
     # 根据轮廓长度进行排序
     sorted_contours = sorted(contours, key=cv2.contourArea, reverse=True)
@@ -30,16 +34,16 @@ def reconstruct_path(binary):
 
     # 对轮廓进行简化或拟合，获得路径点列表
     epsilon = max(0.1 * cv2.arcLength(longest_contour, True), 2)
-    # print(epsilon)
+    # logger.debug(epsilon)
 
     approx = cv2.approxPolyDP(longest_contour, 4, True)
     path = approx.squeeze().tolist()
-    print(path)
+    logger.debug(path)
 
     if path:
         path.append(path[0])
 
-    return path, longest_contour
+    return path, longest_contour.squeeze().tolist()
 
 
 def compute_angle(point1, point2, point3):
@@ -92,7 +96,7 @@ def find_min_angle_vertices(pts):
         raise Exception(f"没有找到两个以上的锐角{angles}")
 
 
-def find_cr(points1, points2):
+def find_center_radius(points1, points2):
     if len(points1) < len(points2):
         points1, points2 = points2, points1
     # 计算距离矩阵
@@ -108,49 +112,44 @@ def find_cr(points1, points2):
     # 计算中点
     midpoint = (points1 + closest_points) / 2
 
-    return np.array(midpoint, np.int32), np.array(min_distances / 2, np.int32)
+    return np.array(midpoint, np.int32), np.array(min_distances / 2, np.int32),closest_points
 
 
-def animation(char, fp="animation.mp4"):
+def animation(char, fp=None, approx=False):
+    """将文字转换成书写GIF"""
+    if len(char) != 1:
+        raise ValueError('only support single character')
     # 读取原始图像
-    output_width, output_height = 150, 150
-    fourcc = cv2.VideoWriter_fourcc(*"mp4v")
-    video_writer = cv2.VideoWriter(fp, fourcc, 30.0, (output_width, output_height))
-    bg = np.zeros((150, 150, 3), np.uint8)
-    frame = np.zeros((150, 150), np.uint8)
-    msk = np.zeros((150, 150), np.uint8)
-    for image, stroke in zip(char_frames(char, vibe=False)[1:], char_strokes(char)):
-        # image = char_frames('国')[4]
+    if not fp:
+        fp = f"{char}.mp4"
 
-        mask = image.point(lambda x: 0 if x > 127 else 255)
-        # mask.show()
+    W, H = 150, 150
+    fourcc = cv2.VideoWriter_fourcc(*"mp4v")
+    video_writer = cv2.VideoWriter(fp, fourcc, 30.0, (W, H))
+    gif_writer = imageio.get_writer(f"{char}.gif", fps=30)
+
+    bg = np.zeros((W, H, 3), np.uint8)
+    key_frame = np.zeros((W, H), np.uint8)
+    for stroke_image, stroke_code in zip(char_frames(char, vibe=False)[1:], char_strokes(char)):
+        msk = np.zeros((W, H), np.uint8)
+        mask = stroke_image.point(lambda x: 0 if x > 127 else 255)
         image = np.asarray(mask, np.uint8)
         # 还原笔记动线
-        frame = cv2.bitwise_or(frame, image)
 
+        # 绘制路径闭合多边形
         path, contours = reconstruct_path(image)
 
-        print(len(path))
-        # 绘制路径
-        # 绘制路径闭合多边形
-        r = 7  # find_inscribed_circle_center(path)[2] #找到多边形的最大○
-        rad = int(r * 2)
-        print(rad)
-        print(path)
-        if path:
-            pts = np.array(path, np.int32)
-            points = path[:-1]
-            angles = find_min_angle_vertices(np.array(points, np.int32))
-            s, e = angles[0][1], angles[1][1]
+        logger.debug(len(path))
 
-            assert points[s] in contours and points[e] in contours
+        r = find_inscribed_circle_center(path)[2]  # 找到多边形的最大○
+        logger.debug(f'max ratio {r}')
 
-            if stroke == "1":
-                if points[s][0] >= points[e][0]:
-                    s, e = e, s
-            else:
-                if points[s][1] >= points[e][1]:  # 除了横以外，一般情况y坐标大的为终点
-                    s, e = e, s
+        points = path[:-1]
+
+        s, e = find_start_end_point(points, stroke_code)
+        start_point, end_point = points[s], points[e]
+
+        if approx:
             if s < e:
                 curves = points[s: e + 1]  # 两条轨迹
                 curves2 = points[e:] + points[: s + 1]
@@ -159,14 +158,9 @@ def animation(char, fp="animation.mp4"):
                 curves = points[s:] + points[: e + 1]
                 curves2 = points[e: s + 1]
                 curves2.reverse()
-                # min_,max_ = e,s
-            contours = [p[0].tolist() for p in contours]
-            print(contours)
-            print(type(points))
-            ss, ee = s, e
+        else:
             s = contours.index(points[s])
             e = contours.index(points[e])
-
             if s < e:
                 curves = contours[s: e + 1]  # 两条轨迹
                 curves2 = contours[e:] + contours[: s + 1]
@@ -176,56 +170,73 @@ def animation(char, fp="animation.mp4"):
                 curves2 = contours[e: s + 1]
                 curves2.reverse()
 
-            print("点数", len(curves), len(curves2))
-            if len(curves) < len(curves2):
-                curves, curves2 = curves, curves2
-            # # 计算所有公切圆
-            centers, rads = find_cr(np.array(curves), np.array(curves2))
-            # print(centers)
-            print(rads)
-            # # 可视化结果
-            # plot_common_tangent_circles(circles, np.array(curves), np.array(curves2))
+        logger.debug("点数", len(curves), len(curves2))
+        if len(curves) < len(curves2):
+            curves, curves2 = curves2, curves
 
-            for i in range(len(centers) - 1):
-                cv2.line(bg, centers[i], centers[i + 1], (0, 255, 255), 1)
-                # cv2.circle(bg, centers[i], rads[i], (0, 0, 255), 1)
+        # # 计算所有公切圆
+        centers, rads, closest_points = find_center_radius(np.array(curves), np.array(curves2))
+        logger.debug(rads)
 
-            for i in range(len(curves) - 1):
-                # 找到 curves[i] 到 curves2 的最短距离
+        pts = np.array(path, np.int32)
+        pts = pts.reshape((-1, 1, 2))
 
-                cv2.line(
-                    msk, curves[i], curves[i + 1], 255, 10 + rads[i] * 2
-                )  # todo 半径要随着位置变化
-                cv2.line(
-                    bg, curves[i], curves[i + 1], (255, 255, 255), 10 + rads[i] * 2
-                )
-                cv2.line(bg, curves[i], curves[i + 1], (255, 0, 0), 2)
-                cv2.circle(bg, centers[i], 1 + int(7 * rads[i]), (0, 0, 255), 1)
-                # cv2.circle(msk,curves[i], 14+rads[i]*2, 255, -1)
-
-                fr = cv2.bitwise_and(frame, msk)
-                video_writer.write(cv2.cvtColor(fr, cv2.COLOR_GRAY2BGR))
-
-            cv2.circle(bg, (points[ss]), 5, (0, 255, 255))  # 起点
-            cv2.circle(bg, (points[ee]), 5, (255, 255, 0))  # 终点
-
-            pts = pts.reshape((-1, 1, 2))
-            cv2.polylines(
-                bg,
-                [pts],
-                True,
-                (
-                    random.randint(0, 255),
-                    random.randint(0, 255),
-                    random.randint(0, 255),
-                ),
-                1,
-            )
-            cv2.fillPoly(bg, [pts], (random.randint(0,255), random.randint(0,255), random.randint(0,255)))
-
-        # 显示结果
+        cv2.fillPoly(bg, [pts], (255, 255, 255))
+        cv2.polylines(
+            bg,
+            [pts],
+            True,
+            (255, 0, 0),
+            1,
+        )
         cv2.imshow("Original Image", bg)
-        cv2.waitKey(0)
+        cv2.circle(bg, start_point, 5, (0, 255, 255))  # 起点
+        cv2.circle(bg, end_point, 5, (255, 255, 0))  # 终点
+
+        for i in range(len(curves)):
+            # 找到 curves[i] 到 curves2 的最短距离
+            cv2.line(msk, curves[i], closest_points[i], 255, 7)  # todo 半径要随着位置变化
+            # cv2.line(bg, curves[i], curves[i + 1], (0, 255, 0), 10 + rads[i] * 2)
+            # cv2.line(bg, curves[i], curves[i + 1], (255, 0, 0), 2)
+            cv2.line(bg, curves[i], closest_points[i], (255, 0, 0), 5)
+
+            # cv2.circle(bg, centers[i], 1 + int(7 * rads[i]), (0, 0, 255), 1)
+            fr = cv2.bitwise_and(image, msk)
+            key_frame = cv2.bitwise_or(key_frame, fr)  # fixme
+
+            video_writer.write(cv2.cvtColor(key_frame, cv2.COLOR_GRAY2BGR))
+            # 写入gif文件中
+            gif_writer.append_data(cv2.cvtColor(key_frame, cv2.COLOR_BGR2RGB))
+            # cv2.imshow("Original Image", bg)
+            # cv2.waitKey(0)
+
+        for i in range(len(centers) - 1):
+            cv2.line(bg, centers[i], centers[i + 1], (0, 255, 255), 2)
+
+        # # 显示结果
+        # cv2.imshow("Original Image", bg)
+        # cv2.waitKey(0)
     cv2.destroyAllWindows()
-    cv2.imwrite("rui.png", bg)
+    cv2.imwrite(f"{char}.png", bg)
     video_writer.release()
+    gif_writer.close()
+
+
+def find_start_end_point(points, stroke_code):
+    """找到points中的起点和终点index"""
+    angles = find_min_angle_vertices(np.array(points, np.int32))
+    s, e = angles[0][1], angles[1][1]
+    # assert points[s] in contours and points[e] in contours
+    if stroke_code == "1":
+        if points[s][0] >= points[e][0]:
+            s, e = e, s
+    else:
+        if points[s][1] >= points[e][1]:  # 除了横以外，一般情况y坐标大的为终点
+            s, e = e, s
+    return s, e
+
+
+if __name__ == '__main__':
+    import sys
+
+    animation(sys.argv[1])
